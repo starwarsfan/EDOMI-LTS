fc1d8285eba0a72a3e480933e921033ac21f15fa
Yves Schumann <y.schumann@proton.me>
2024-07-09 14:08:24 +0200

    Format script

--- include/install_RockyLinux8.sh
+++ include/install_RockyLinux8.sh
@@ -10,13 +10,13 @@
 # Store path from where script was called,
 # determine own location and cd there
 callDir=$(pwd)
-ownLocation="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ownLocation="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd ${ownLocation}
 . ./helpers_console.sh
 _init
 
 # Determine architecture
-if [ $(uname -m) = 'aarch64' ] ; then
+if [ $(uname -m) = 'aarch64' ]; then
     ARCH_SUFFIX="-aarch64"
 fi
 
@@ -76,11 +76,11 @@ dnf install -y \
     dnf-utils
 dnf clean all
 rm -f /etc/vsftpd/ftpusers \
-      /etc/vsftpd/user_list
+    /etc/vsftpd/user_list
 info " -> Done"
 
 info "Installing LBS requirements"
-for currentInclude in ${ownLocation}/lbs-requirements/* ; do
+for currentInclude in ${ownLocation}/lbs-requirements/*; do
     info " -> Loading/executing include ${currentInclude}"
     . ${currentInclude}
 done
@@ -107,7 +107,7 @@ localectl set-keymap de-nodeadkeys
 timedatectl set-timezone Europe/Berlin
 
 # Configure mariadb
-cat << EOF > /tmp/tmp.txt
+cat <<EOF >/tmp/tmp.txt
 key_buffer_size=256M
 sort_buffer_size=8M
 read_buffer_size=16M
@@ -125,7 +125,7 @@ EOF
 # like this will fail because of the empty values:
 # INSERT INTO edomiProject.editLogicCmdList (targetid,cmd,cmdid1,cmdid2,cmdoption1,cmdoption2,cmdvalue1,cmdvalue2) \
 #                                    VALUES ('2',    '1', '101', '',    '',        '',        null,     null)
-echo "sql_mode=ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION" >> /tmp/tmp.txt
+echo "sql_mode=ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION" >>/tmp/tmp.txt
 sed -i '/\[mysqld\]/r /tmp/tmp.txt' /etc/my.cnf.d/mariadb-server.cnf
 info " -> Done"
 
@@ -153,7 +153,7 @@ mysql -e "FLUSH PRIVILEGES;"
 mysql -e "GRANT ALL ON *.* TO mysql@'%';"
 
 # MySQL symlink
-echo "Alias=mysqld.service" 			> /tmp/tmp.txt
+echo "Alias=mysqld.service" >/tmp/tmp.txt
 sed -i '/\[Install\]/r /tmp/tmp.txt' /usr/lib/systemd/system/mariadb.service
 ln -s '/usr/lib/systemd/system/mariadb.service' '/etc/systemd/system/mysqld.service'
 systemctl daemon-reload
@@ -181,8 +181,8 @@ info " -> Done"
 info "Installing MySQL UDF log/sys"
 # Enable lib_mysqludf_sys
 systemctl start mariadb
-mysql -u root mysql < ${ownLocation}/scripts/installdb_mysqludf_log.sql
-mysql -u root mysql < ${ownLocation}/scripts/installdb_mysqludf_sys.sql
+mysql -u root mysql <${ownLocation}/scripts/installdb_mysqludf_log.sql
+mysql -u root mysql <${ownLocation}/scripts/installdb_mysqludf_sys.sql
 info " -> Done"
 
 info "Tweak some default settings"
